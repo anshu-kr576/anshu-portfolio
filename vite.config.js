b435971bd7c65ba35709e3a780ea87fc305d857e
anshukr-576 <anshuking576@gmail.com>
2026-01-13 17:20:19 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,31 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   base: '/PortFolio/',
-//    resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// });
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
-
-export default defineConfig(({ command }) => ({
+// https://vite.dev/config/
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: command === "build" ? "/PortFolio/" : "/",
-  resolve: {
+  base: '/PortFolio/',
+   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import path from "path";
+
+// export default defineConfig(({ command }) => ({
+//   plugins: [react(), tailwindcss()],
+//   base: command === "build" ? "/PortFolio/" : "/",
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
 
